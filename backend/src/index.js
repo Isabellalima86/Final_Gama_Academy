9e8e4e45571aed121dec7ad8bb8b2fae41ba533f
isabellalima86 <isabellalimasilva@gmail.com>
2021-08-30 07:50:57 -0300

    alterações da manhã

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.json');
-const routes = require('../routes');
+const routes = require('./routes');
 const app = express();
 const cors = require('cors');
 
@@ -15,6 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
-app.listen('5000', () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('rodando na porta 5000');
 });
